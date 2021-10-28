17bd1f23a3a275df4ab59c44b20b5778ff909c94
Daniel Songi <daniel.songi10@gmail.com>
2021-10-28 20:37:15 +0100

    refactored verbose error matches

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,7 @@ use linalg::{Matrix, Error};
 /// 
 /// The new 'w' value in the direction of gradient decent
 fn linear_regression_step(data: &Matrix<f64>, solution: &Matrix<f64>, w: &Matrix<f64>, step_length: f64) -> Result<Matrix<f64>, Error> {
-	let gradient = match mse_gradient(data, solution, w) {
-		Ok(g) => g,
-		Err(e) => return Err(e),
-	};
+	let gradient = mse_gradient(data, solution, w)?;
 	let step = gradient.mul_scalar(-2.0 * step_length);
 	w.add(&step)
 }
@@ -26,23 +23,14 @@ fn mse_gradient(data: &Matrix<f64>, solution: &Matrix<f64>, w: &Matrix<f64>) -> 
 	let height = data.height();
 	let mut sum = Matrix::new(w.width(), w.height());
 	for i in 0..height {
-		let row = match data.row(i) {
-			Ok(r) => r,
-			Err(e) => return Err(e)
-		};
-		let solution_element = match solution.get(0, i) {
-			Ok(element) => element,
-			Err(e) => return Err(e)
-		};
+		let row = data.row(i)?;
+		let solution_element = solution.get(0, i)?;
 		let coef = match row.dot(w) {
 			Ok(scalar) => scalar - solution_element,
 			Err(e) => return Err(e)
 		};
 		let value = row.mul_scalar(coef);
-		sum = match sum.add(&value) {
-			Ok(vector) => vector,
-			Err(e) => return Err(e)
-		};
+		sum = sum.add(&value)?;
 	}
 	Ok(sum.mul_scalar(1.0 / height as f64))
 }
@@ -61,18 +49,13 @@ fn soft_threshold(matrix: &Matrix<f64>, lambda: f64) -> Result<Matrix<f64>, Erro
 	let mut thresholded_matrix = Matrix::new(matrix.width(), matrix.height());
 	for i in 0..matrix.width() {
 		for j in 0..matrix.height() {
-			let thresholded_element = match matrix.get(i, j) {
-				Ok(element) => match element {
-					e if e > lambda => e - lambda,
-					e if e < -lambda => e + lambda,
-					_ => 0.0,
-				},
-				Err(e) => return Err(e)
+			let element = matrix.get(i, j)?;
+			let thresholded_element = match element {
+				e if e > lambda => e - lambda,
+				e if e < -lambda => e + lambda,
+				_ => 0.0,
 			};
-			match thresholded_matrix.set(i, j, thresholded_element) {
-				Ok(_) => (),
-				Err(e) => return Err(e)
-			};
+			thresholded_matrix.set(i, j, thresholded_element)?;
 		}
 	}
 	Ok(thresholded_matrix)
@@ -92,10 +75,7 @@ fn soft_threshold(matrix: &Matrix<f64>, lambda: f64) -> Result<Matrix<f64>, Erro
 /// 
 /// The new 'w' value in the direction reducing MSE + 1-norm (might not reduce if it overshoots)
 fn lasso_step(data: &Matrix<f64>, solution: &Matrix<f64>, w: &Matrix<f64>, step_length: f64, lambda: f64) -> Result<Matrix<f64>, Error> {
-	let linear_regression_w = match linear_regression_step(data, solution, w, step_length) {
-		Ok(vector) => vector,
-		Err(e) => return Err(e),
-	};
+	let linear_regression_w = linear_regression_step(data, solution, w, step_length)?;
 	soft_threshold(&linear_regression_w, lambda)
 }
 
@@ -116,10 +96,7 @@ fn lasso_step(data: &Matrix<f64>, solution: &Matrix<f64>, w: &Matrix<f64>, step_
 pub fn simple_lasso(data: &Matrix<f64>, solution: &Matrix<f64>, initial_w: &Matrix<f64>, step_length: f64, lambda: f64, steps: usize) -> Result<Matrix<f64>, Error> {
 	let mut w = initial_w.clone();
 	for _ in 0..steps {
-		w = match lasso_step(data, solution, &w, step_length, lambda) {
-			Ok(new_w) => new_w,
-			Err(e) => return Err(e),
-		}
+		w = lasso_step(data, solution, &w, step_length, lambda)?
 	}
 	Ok(w)
 }
